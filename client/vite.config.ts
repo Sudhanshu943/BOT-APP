fcb474a8564684e6308d65178947e1381dd3a5ad
Sudhanshu Thapa <sudhanshuthapa8@gmail.com>
2025-05-03 15:21:27 +0530

    chan

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: '../dist/client', // optional, based on your deployment structure
-  }
+  plugins: [react(), tsconfigPaths()]
 })
